99ae9bc17b65d54ed335566d894462506c81b6c7
Alex Law <achlawuk@gmail.com>
2017-10-08 15:43:36 +0100

    Text inout adds new to do

--- assets/js/to_do_list.js
+++ assets/js/to_do_list.js
@@ -8,3 +8,11 @@ $("span").click(function(clicked){
   });
   clicked.stopPropagation();
 });
+
+$("input[type='text']").keypress(function(clicked){
+  if(clicked.which === 13){
+    var newToDo = $(this).val();
+    $(this).val("");
+    $("ul").append("<li><span>Delete</span> " + newToDo + "</li>")
+  }
+});
